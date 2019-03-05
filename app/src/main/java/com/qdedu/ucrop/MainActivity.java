ff4c197975dee677c92ede7c2433ddc9a86b2bb7
qdzhl <pengcf@qdedu.cn>
2019-03-05 14:33:56 +0800

    first  commit

---
+++ app/src/main/java/com/qdedu/ucrop/MainActivity.java
@@ -0,0 +1,13 @@
+package com.qdedu.ucrop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
